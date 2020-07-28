6bf7c6ae2db3682100f7b202862207cacc82d82b
yellowglasses20 <yano.makoto20@gmail.com>
2020-07-28 17:08:33 +0900

    refactoring

--- main.go
+++ main.go
@@ -20,40 +20,20 @@ func run() int {
 	serviceList := tview.NewList()
 	serviceList.SetTitle("- file list -").SetBorder(true)
 
-	bucketList := tview.NewTextView()
-	bucketList.SetTitle("- Text -").SetBorder(true)
+	textView := tview.NewTextView()
+	textView.SetTitle("- Text -").SetBorder(true)
 
 	flex := tview.NewFlex().
 		AddItem(serviceList, 20, 0, true).
-		AddItem(bucketList, 0, 1, false)
+		AddItem(textView, 0, 1, false)
 
-	drawServiceList(serviceList, bucketList, app)
+	drawServiceList(serviceList, textView, app)
 
 	if err := app.SetRoot(flex, true).EnableMouse(true).Run(); err != nil {
 		panic(err)
 	}
 
 	return 0
-}
-
-func bucketFunc(bucketList *tview.TextView, f string, app *tview.Application) {
-	bucketList.Clear()
-	text, err := ioutil.ReadFile(f)
-	if err != nil {
-		panic(err)
-	}
-	bucketList.SetTitle(f)
-	bucketList.SetText(string(text))
-	// app.SetFocus(bucketList)
-}
-
-func dirwalk(dir string) []os.FileInfo {
-	files, err := ioutil.ReadDir(dir)
-	if err != nil {
-		panic(err)
-	}
-
-	return files
 }
 
 func drawServiceList(serviceList *tview.List, bucketList *tview.TextView, app *tview.Application) {
@@ -66,14 +46,6 @@ func createServiceList(serviceList *tview.List, bucketList *tview.TextView, file
 	serviceList.Clear()
 	upDir(serviceList, bucketList, app, dir)
 	selectServiceList(serviceList, bucketList, fileInfo, app, dir)
-}
-
-func dirAbs(path string) string {
-	p, err := filepath.Abs(path)
-	if err != nil {
-		log.Fatal(err)
-	}
-	return p
 }
 
 func upDir(serviceList *tview.List, bucketList *tview.TextView, app *tview.Application, dir string) {
@@ -100,17 +72,45 @@ func selectServiceList(serviceList *tview.List, bucketList *tview.TextView, file
 	}
 }
 
-func fileStat(filePath string) (os.FileInfo, error) {
-	f, _ := os.Open(filePath)
-	defer f.Close()
-	return f.Stat()
-}
-
 func selectDirOrFile(fs os.FileInfo, nextFilePath string, serviceList *tview.List, bucketList *tview.TextView, app *tview.Application) {
 	if fs.IsDir() {
 		f := dirwalk(nextFilePath)
 		createServiceList(serviceList, bucketList, f, app, nextFilePath)
 		return
 	}
-	bucketFunc(bucketList, nextFilePath, app)
+	createTextView(bucketList, nextFilePath, app)
+}
+
+func createTextView(bucketList *tview.TextView, f string, app *tview.Application) {
+	bucketList.Clear()
+	text, err := ioutil.ReadFile(f)
+	if err != nil {
+		panic(err)
+	}
+	bucketList.SetTitle(f)
+	bucketList.SetText(string(text))
+	// app.SetFocus(bucketList)
+}
+
+func dirwalk(dir string) []os.FileInfo {
+	files, err := ioutil.ReadDir(dir)
+	if err != nil {
+		panic(err)
+	}
+
+	return files
+}
+
+func dirAbs(path string) string {
+	p, err := filepath.Abs(path)
+	if err != nil {
+		log.Fatal(err)
+	}
+	return p
+}
+
+func fileStat(filePath string) (os.FileInfo, error) {
+	f, _ := os.Open(filePath)
+	defer f.Close()
+	return f.Stat()
 }
